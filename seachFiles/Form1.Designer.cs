1046cbb8b5f236a961a54d47213053e23b99cce1
RindonS207 <ld3406255407@outlook.com>
2023-01-23 19:57:56 +0800

    searchFiles

---
+++ seachFiles/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace seachFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(534, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(241, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(400, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "要搜索的文件夹";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(794, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "浏览...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "无筛选要求",
+            "500MB以上",
+            "1GB以上",
+            "5GB以上",
+            "10GB以上"});
+            this.comboBox1.Location = new System.Drawing.Point(629, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Azure;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.richTextBox1.Location = new System.Drawing.Point(79, 146);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(1165, 338);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(794, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "开始搜索";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "检索文件夹";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(634, 495);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(249, 23);
+            this.progressBar1.Step = 25;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(495, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "查找多大的文件";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(563, 498);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "进度条^^";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1291, 533);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "搜索大文件";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ seachFiles/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace seachFiles
+{
+    public partial class Form1 : Form
+    {
+        List<string> fileName = new List<string>();
+        StringBuilder printAllInfo = new StringBuilder();
+        public string printFileLength(long fileByte)
+        {
+            if (fileByte < 1024)
+            {
+                return "" + fileByte + " 字节";
+            }
+            else if (fileByte >= 1024 && fileByte < 1048576)
+            {
+                return "" + (Math.Round(Convert.ToDouble(fileByte)/ 1024,3)) + " KB";
+            }
+            else if (fileByte >= 1048576 && fileByte < 1073741824)
+            {
+                return "" + (Math.Round(Convert.ToDouble(fileByte) / 1048576, 3)) + " MB";
+            }
+            else
+            {
+                return "" + (Math.Round(Convert.ToDouble(fileByte) / 1073741824, 3)) + " GB";
+            }
+        }
+        public long getFileLength(string filePath)
+        {
+            FileInfo fi = new FileInfo(filePath);
+            long len;
+            len = fi.Length;
+            fi = null;
+            return len;
+        }
+        public void printRichBox()
+        {
+            printAllInfo.Clear();
+            foreach(var fileN in fileName)
+            {
+                try
+                {
+                    FileInfo fi = new FileInfo(fileN);
+                    if (comboBox1.Text == "" || comboBox1.Text == "无筛选要求")
+                    {
+                        printAllInfo.Append("\n文件：" + fileN + "  大小：" + printFileLength(fi.Length) + "\n");
+                    }
+                    else if (comboBox1.Text == "500MB以上" && fi.Length >= 524288000)
+                    {
+                        printAllInfo.Append("\n文件：" + fileN + "  大小：" + printFileLength(fi.Length) + "\n");
+                    }
+                    else if (comboBox1.Text == "1GB以上" && fi.Length >= 1073741824)
+                    {
+                        printAllInfo.Append("\n文件：" + fileN + "  大小：" + printFileLength(fi.Length) + "\n");
+                    }
+                    else if (comboBox1.Text == "5GB以上" && fi.Length >= 5368709120)
+                    {
+                        printAllInfo.Append("\n文件：" + fileN + "  大小：" + printFileLength(fi.Length) + "\n");
+                    }
+                    else if (comboBox1.Text == "10GB以上" && fi.Length >= 10737418240)
+                    {
+                        printAllInfo.Append("\n文件：" + fileN + "  大小：" + printFileLength(fi.Length) + "\n");
+                    }
+                    fi = null;
+                }
+                catch(Exception ex)
+                {
+                    richTextBox1.Text = "错误！" + ex.Message + "错误文件：" + fileN + "\n";
+                }
+            }
+            progressBar1.PerformStep();
+            if(printAllInfo.ToString() == "")
+            {
+                richTextBox1.Text = "未能找到符合条件的内容！也可能是文件夹里全是不大不小的文件，未达到您的筛选要求！";
+                progressBar1.PerformStep();
+            }
+            else
+            {
+                richTextBox1.Text += printAllInfo;
+                progressBar1.PerformStep();
+            }
+        }
+        public void folerSeach(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileSystemInfo[] fsif = dir.GetFileSystemInfos();
+            foreach(var x in fsif)
+            {
+                if(x is DirectoryInfo)
+                {
+                    folerSeach(x.FullName);
+                }
+                else
+                {
+                    fileName.Add(x.FullName);
+                }
+            }
+            progressBar1.PerformStep();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("请选择一个文件夹路径。");
+            }
+            else
+            {
+                progressBar1.Value = 0;
+                fileName.Clear();
+                richTextBox1.Text = "正在检索中，如果文件太多会导致检索速度很慢....";
+                progressBar1.PerformStep();
+                folerSeach(textBox1.Text);
+                printRichBox();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            comboBox1.Location = new Point(this.Width / 2,(this.Height / 2) - 150);
+            label1.Location = new Point(label2.Location.X - 95, label2.Location.Y - 54);
+            textBox1.Location = new Point(label2.Location.X , label2.Location.Y - 58);
+            button1.Location = new Point(label2.Location.X + 190, label2.Location.Y - 58);
+            label2.Location = new Point((this.Width / 2) - 100, (this.Height / 2) - 148);
+            button2.Location = new Point((this.Width / 2) + 105, (this.Height / 2) - 152);
+            progressBar1.Location = new Point((this.Width / 2) - 50, richTextBox1.Location.Y + richTextBox1.Size.Height + 10);
+            label3.Location = new Point((this.Width / 2) - 115, richTextBox1.Location.Y + richTextBox1.Size.Height + 12);
+            richTextBox1.Size = new Size(this.Width - 150,this.Height / 2);
+            richTextBox1.Location = new Point(richTextBox1.Location.X,comboBox1.Location.Y + 50);
+        }
+    }
+}
